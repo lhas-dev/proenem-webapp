523c9b46410709a13507a61e79ec4b98c62d2cf3
lhas <luizhrqas@gmail.com>
2019-03-27 12:04:00 -0300

    Initialized redux structure

---
+++ src/store/reducers/auth.js
@@ -0,0 +1,18 @@
+const initialState = {
+    email: '',
+    token: '',
+};
+
+const auth = (state = initialState, action) => {
+    switch (action.type) {
+        case 'AUTHENTICATION_SUCCESS':
+            return {
+                ...state,
+                ...action.payload
+            };
+        default:
+            return state;
+    }
+};
+
+export default auth;
